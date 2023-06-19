d8496dcbaf7d407e247323cb37ef48b060820cce
simon-jo8 <112388673+simon-jo8@users.noreply.github.com>
2023-06-19 10:22:18 +0200

    Exo2

--- src/script.js
+++ src/script.js
@@ -9,14 +9,24 @@ const sizes = {
 const canvas = document.querySelector('#three');
 
 // LIGHT
-var ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( ambientLight );
+// var ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
+// scene.add( ambientLight );
+
+//SPOTLIGHT
+var spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(0, 10, 0);
+spotLight.angle = Math.PI/4;
+spotLight.distance = 50;
+spotLight.castShadow = true;
 
 
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-directionalLight.position.set(0, 10, 0); // set the light direction
-directionalLight.castShadow = true; // enable shadow for the light
-scene.add( directionalLight );
+scene.add(spotLight);
+
+// DIR LIGHT
+// var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+// directionalLight.position.set(0, 10, 0); // set the light direction
+// directionalLight.castShadow = true; // enable shadow for the light
+// scene.add( directionalLight );
 
 
 // Camera
@@ -36,21 +46,38 @@ plane.rotation.x = Math.PI / 2;
 
 scene.add( plane );
 
-// SPHERE
+// Textures
+const textureLoader = new THREE.TextureLoader();
+const metal = textureLoader.load('texture/metal.jpeg');
+const wood = textureLoader.load('texture/wood.jpeg');
+
+// SPHERES
 
 var sphereGeometry = new THREE.SphereGeometry( 1, 32, 32 );
 
-var sphere1 = new THREE.Mesh( sphereGeometry, new THREE.MeshStandardMaterial( { color: 0xff0000 } ) );
+var sphere1Material = new THREE.MeshStandardMaterial({
+    map: metal,
+    metalness: 0.5, // Controls how "metal-like" the material is. 0.0 is not metal at all, 1.0 is 100% metal.
+    roughness: 0.5  // Controls the roughness of the material. Lower values make the material appear smoother and shinier, higher values make it appear rougher and less shiny.
+});
+
+var sphere1 = new THREE.Mesh( sphereGeometry, sphere1Material );
 sphere1.position.set(-3, 2, 0);
 sphere1.castShadow = true;
 scene.add( sphere1 );
 
-var sphere2 = new THREE.Mesh( sphereGeometry, new THREE.MeshStandardMaterial( { color: 0x00ff00 } ) );
+var sphere2 = new THREE.Mesh( sphereGeometry, new THREE.MeshStandardMaterial( { color: 0xff0000 } ) );
 sphere2.position.set(0, 1, 0);
 sphere2.castShadow = true;
 scene.add( sphere2 );
 
-var sphere3 = new THREE.Mesh( sphereGeometry, new THREE.MeshStandardMaterial( { color: 0x0000ff } ) );
+var sphere2Material = new THREE.MeshStandardMaterial({
+    map: wood,
+    metalness: 0.5, // Controls how "metal-like" the material is. 0.0 is not metal at all, 1.0 is 100% metal.
+    roughness: 0.5  // Controls the roughness of the material. Lower values make the material appear smoother and shinier, higher values make it appear rougher and less shiny.
+});
+
+var sphere3 = new THREE.Mesh( sphereGeometry, sphere2Material );
 sphere3.position.set(3, 3, 0);
 sphere3.castShadow = true;
 scene.add( sphere3 );
